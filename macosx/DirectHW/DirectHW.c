456b4179300d09d1212fed1bf350d3188c072a53
Stefan Tauner <stefan.tauner@gmx.at>
2016-03-05 19:50:40 +0100

    osx: fix some compiler warnings

--- macosx/DirectHW/DirectHW.c
+++ macosx/DirectHW/DirectHW.c
@@ -21,6 +21,7 @@
 #include <ApplicationServices/ApplicationServices.h>
 #include <CoreFoundation/CoreFoundation.h>
 #include <unistd.h>
+#include <inttypes.h>
 #include <errno.h>
 #include "DirectHW.h"
 
@@ -272,7 +273,7 @@ void *map_physical(uint64_t phys_addr, size_t len)
 		printf("\nError(kPrepareMap): system 0x%x subsystem 0x%x code 0x%x ",
 				err_get_system(err), err_get_sub(err), err_get_code(err));
 
-		printf("physical 0x%08lx[0x%x]\n", phys_addr, (unsigned int)len);
+		printf("physical 0x%"PRIx64"[0x%x]\n", phys_addr, (unsigned int)len);
 
 		switch (err_get_code(err)) {
 		case 0x2c2: printf("Invalid argument.\n"); errno = EINVAL; break;
@@ -295,7 +296,7 @@ void *map_physical(uint64_t phys_addr, size_t len)
 		printf("\nError(IOConnectMapMemory): system 0x%x subsystem 0x%x code 0x%x ",
 				err_get_system(err), err_get_sub(err), err_get_code(err));
 
-		printf("physical 0x%08lx[0x%x]\n", phys_addr, (unsigned int)len);
+		printf("physical 0x%"PRIx64"[0x%x]\n", phys_addr, (unsigned int)len);
 
 		switch (err_get_code(err)) {
 		case 0x2c2: printf("Invalid argument.\n"); errno = EINVAL; break;
@@ -384,5 +385,6 @@ int wrmsr(int addr, msr_t msr)
 int logical_cpu_select(int cpu)
 {
 	current_logical_cpu = cpu;
+	return 0;
 }
 
